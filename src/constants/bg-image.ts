a70c11328a7eb825cf2f452603aa9b8b7935ff42
MidnightCoke <jimmyfallon717@gmail.com>
2025-06-01 15:48:37 +0300

    Fix import paths in bg-image.ts and weather-conditions.ts to use lowercase 'images'

--- src/constants/bg-image.ts
+++ src/constants/bg-image.ts
@@ -1,9 +1,9 @@
-import Clear from "../../assets/Images/Clear.png";
-import Clouds from "../../assets/Images/Clouds.png";
-import Mist from "../../assets/Images/Mist.png";
-import Rain from "../../assets/Images/Rain.png";
-import Snow from "../../assets/Images/Snow.png";
-import Thunderstorm from "../../assets/Images/Thunderstorm.png";
+import Clear from "../../assets/images/Clear.png";
+import Clouds from "../../assets/images/Clouds.png";
+import Mist from "../../assets/images/Mist.png";
+import Rain from "../../assets/images/Rain.png";
+import Snow from "../../assets/images/Snow.png";
+import Thunderstorm from "../../assets/images/Thunderstorm.png";
 
 const bgImage = {
   Clouds,
